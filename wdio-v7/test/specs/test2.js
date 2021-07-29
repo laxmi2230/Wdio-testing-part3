afe6b1dd38ea0f1269691d7002fcff51f1f0817a
laxmi menon <46654095+laxmi2230@users.noreply.github.com>
2021-07-29 11:55:51 +0530

    Add files via upload

---
+++ wdio-v7/test/specs/test2.js
@@ -0,0 +1,11 @@
+describe('works correctly', () => {
+    it('should give correct output', () => {
+       browser.url('http://localhost/dog.html');
+       
+     expect(browser).toHaveTitle('list of songs')
+     
+    //  var = xyz ;
+      //return xyz;
+      
+    })
+})
